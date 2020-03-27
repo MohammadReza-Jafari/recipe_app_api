bb04a995bb439814dd281e2f4b1b48bcf72e3025
MohmmadReza-Jafari <chashm78@gmail.com>
2020-03-28 00:16:23 +0430

    create user manager and auth user model and test them

---
+++ core/tests/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """test creating user with an email"""
+        email = 'chashm78@gmail.com'
+        password = 'django1234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_normalized_email(self):
+        email = 'chashm78@GMAIL.COM'
+        password = 'django1234'
+        user = get_user_model().objects.create_user(email=email, password=password)
+        self.assertEqual(email.lower(), user.email)
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'password1234')
+
+    def test_create_new_superuser(self):
+        """test create new super user"""
+        user = get_user_model().objects.create_superuser('chashm78@gmail.com', 'django1234')
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
